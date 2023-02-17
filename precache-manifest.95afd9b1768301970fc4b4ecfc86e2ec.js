91d2021205f4921a589ffdec0b666eebb50e056b
Jiří Glončák <georgincz@seznam.cz>
2023-02-17 17:15:04 +0100

    Updates

---
+++ precache-manifest.95afd9b1768301970fc4b4ecfc86e2ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab650c8d93bbf48c2969",
+    "url": "/web-dev/static/css/main.00098dbb.chunk.css"
+  },
+  {
+    "revision": "ab650c8d93bbf48c2969",
+    "url": "/web-dev/static/js/main.ab650c8d.chunk.js"
+  },
+  {
+    "revision": "c6bf8d04a26a23246908",
+    "url": "/web-dev/static/js/1.c6bf8d04.chunk.js"
+  },
+  {
+    "revision": "68258330e00a2c20a45f",
+    "url": "/web-dev/static/js/runtime~main.68258330.js"
+  },
+  {
+    "revision": "0863b5f19e284078acd8693ed0140d6b",
+    "url": "/web-dev/index.html"
+  }
+];
